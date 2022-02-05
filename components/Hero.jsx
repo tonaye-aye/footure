7176d1087b10570f5f340ec266072f3fdd70de36
Tony Gorgioski <tony.gorgioski@gmail.com>
2022-02-06 00:49:12 +1100

    done for today

---
+++ components/Hero.jsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import heroImage from '../public/assets/hero.svg'
+
+export default function Hero() {
+  return (
+    <section className="mt-12 mb-4 sm:mt-24 sm:mb-12 px-4 grid items-center grid-cols-3 gap-x-4 sm:gap-x-24">
+      <h1 className="col-span-3 sm:col-span-2 font-bold text-white text-xl lg:text-3xl">
+        Navigate the latest fixtures, results and tips on everything{' '}
+        <span className="text-blue-600">football.</span>
+      </h1>
+      <div className="col-span-3 sm:col-span-1 flex justify-center">
+        <Image src={heroImage} width={200} height={200} alt="Soccer Goal" />
+      </div>
+    </section>
+  )
+}
---
+++ components/TabsHome.jsx
@@ -0,0 +1,108 @@
+import { useState } from 'react'
+import { Tab } from '@headlessui/react'
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function TabsHome() {
+  let [categories] = useState({
+    Recent: [
+      {
+        id: 1,
+        title: 'Does drinking coffee make you smarter?',
+        date: '5h ago',
+        commentCount: 5,
+        shareCount: 2
+      },
+      {
+        id: 2,
+        title: "So you've bought coffee... now what?",
+        date: '2h ago',
+        commentCount: 3,
+        shareCount: 2
+      }
+    ],
+    Popular: [
+      {
+        id: 1,
+        title: 'Is tech making coffee better or worse?',
+        date: 'Jan 7',
+        commentCount: 29,
+        shareCount: 16
+      },
+      {
+        id: 2,
+        title: 'The most innovative things happening in coffee',
+        date: 'Mar 19',
+        commentCount: 24,
+        shareCount: 12
+      }
+    ]
+  })
+
+  return (
+    <section className="px-4">
+      <Tab.Group>
+        <Tab.List className="w-full max-w-sm flex p-1 space-x-1 bg-blue-900/20 rounded-xl">
+          {Object.keys(categories).map((category) => (
+            <Tab
+              key={category}
+              className={({ selected }) =>
+                classNames(
+                  'w-full py-2.5 text-sm leading-5 font-medium text-blue-200 rounded-lg transition ease-in duration-300',
+                  'focus:outline-none focus:ring-2 ring-offset-1 ring-offset-blue-600 ring-gray-900 ring-opacity-60',
+                  selected
+                    ? 'text-white bg-gray-900 shadow'
+                    : 'text-blue-400 hover:bg-gray-800 hover:text-white'
+                )
+              }
+            >
+              {category}
+            </Tab>
+          ))}
+        </Tab.List>
+        <Tab.Panels className="mt-2">
+          {Object.values(categories).map((posts, idx) => (
+            <Tab.Panel
+              key={idx}
+              className={classNames(
+                'bg-gray-400 rounded-xl p-3',
+                'focus:outline-none focus:ring-2 ring-offset-2 ring-offset-blue-400 ring-white ring-opacity-60'
+              )}
+            >
+              <ul>
+                {posts.map((post) => (
+                  <li
+                    key={post.id}
+                    className="relative p-3 rounded-md hover:bg-coolGray-100"
+                  >
+                    <h3 className="text-sm font-medium leading-5">
+                      {post.title}
+                    </h3>
+
+                    <ul className="flex mt-1 space-x-1 text-xs font-normal leading-4 text-coolGray-500">
+                      <li>{post.date}</li>
+                      <li>&middot;</li>
+                      <li>{post.commentCount} comments</li>
+                      <li>&middot;</li>
+                      <li>{post.shareCount} shares</li>
+                    </ul>
+
+                    <a
+                      href="#"
+                      className={classNames(
+                        'absolute inset-0 rounded-md',
+                        'focus:z-10 focus:outline-none focus:ring-2 ring-blue-400'
+                      )}
+                    />
+                  </li>
+                ))}
+              </ul>
+            </Tab.Panel>
+          ))}
+        </Tab.Panels>
+      </Tab.Group>
+    </section>
+  )
+}
