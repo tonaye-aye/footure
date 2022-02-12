9759adb86a312187b85f63432046ad63725272de
Tony Gorgioski <tony.gorgioski@gmail.com>
2022-02-12 23:00:25 +1100

    fixing

--- layouts/container.jsx
+++ layouts/container.jsx
@@ -6,7 +6,7 @@ export default function Layout({ title, children }) {
   return (
     <>
       <Head>
-        <title>FootballFriend | {title}</title>
+        <title>FootballFriend - {title}</title>
         <meta name="description" content="The simple football directory." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
